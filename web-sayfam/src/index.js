c6620f330293665a1de4e6a44a56b086db5574f5
derya <deryabalin@gmail.com>
2023-07-21 02:07:04 +0300

    proje tamamlandı

--- web-sayfam/src/index.js
+++ web-sayfam/src/index.js
@@ -4,16 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import i18n from "./components /i18n";
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </BrowserRouter>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
